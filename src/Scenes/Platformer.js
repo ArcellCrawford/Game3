d6c4d2463a9a1f6ef0118e9746ff49e4a7d237e7
Arcell Cradford <arcell2002@gmail.com>
2024-05-31 21:53:30 -0700

    commit

--- src/Scenes/Platformer.js
+++ src/Scenes/Platformer.js
@@ -12,7 +12,9 @@ class Platformer extends Phaser.Scene {
         this.PARTICLE_VELOCITY = 50;
         this.SCALE = 2.0;
         this.score = 0 ;
-        this.text = this.add.text(16 + this.cameras.main.scrollX, this.cameras.main.scrollY, 'Score:' + this.score, { fontFamily: 'Arial', fontSize: 18, color: '#ffffff' });
+        this.text
+        this.win = false
+        //this.text = this.add.text(16 + this.cameras.main.scrollX, this.cameras.main.scrollY, 'Score:' + this.score, { fontFamily: 'Arial', fontSize: 18, color: '#ffffff' });
        
 
     }
@@ -30,7 +32,7 @@ class Platformer extends Phaser.Scene {
         
         // Create a new tilemap game object which uses 18x18 pixel tiles, and is
         // 45 tiles wide and 25 tiles tall.
-        this.map = this.add.tilemap("platformer-level-1", 18, 18, 45, 25);
+        this.map = this.add.tilemap("platformer-level-1", 18, 18, 81, 25);
 
         // Add a tileset to the map
         // First parameter: name we gave the tileset in Tiled
@@ -39,7 +41,7 @@ class Platformer extends Phaser.Scene {
 
         // Create a layer
         this.groundLayer = this.map.createLayer("Ground-n-Platforms", this.tileset, 0, 0);
-
+        this.waterLayer = this.map.createLayer("Water",this.tileset,0,0)
         // Make it collidable
         this.groundLayer.setCollisionByProperty({
             collides: true
@@ -69,6 +71,18 @@ class Platformer extends Phaser.Scene {
             key: "tilemap_sheet",
             frame: 128,     
         });
+
+        this.powerup1 = this.map.createFromObjects("Powerup2", {
+            name: "DashVine",
+            key: "tilemap_sheet",
+            frame: 127,     
+        });
+        
+        this.winflag = this.map.createFromObjects("WinFlag", {
+            name: "WinFlag",
+            key: "tilemap_sheet",
+            frame: 131,     
+        });
         //setting the particles for when a coin is collected
       
        
@@ -78,6 +92,8 @@ class Platformer extends Phaser.Scene {
         // them into Arcade Physics sprites (STATIC_BODY, so they don't move) 
         this.physics.world.enable(this.coins, Phaser.Physics.Arcade.STATIC_BODY);
         this.physics.world.enable(this.powerup, Phaser.Physics.Arcade.STATIC_BODY);
+        this.physics.world.enable(this.powerup1, Phaser.Physics.Arcade.STATIC_BODY);
+        this.physics.world.enable(this.winflag, Phaser.Physics.Arcade.STATIC_BODY);
 
         // Create a Phaser group out of the array this.coins
         // This will be used for collision detection below.
@@ -85,40 +101,47 @@ class Platformer extends Phaser.Scene {
         
         this.powerGroup = this.add.group(this.powerup)
         
+        this.powerGroup2 = this.add.group(this.powerup1)
+        this.winflagGroup = this.add.group(this.winflag)
         // set up player avatar
         my.sprite.player = this.physics.add.sprite(this.spawn[0].x, this.spawn[0].y, "platformer_characters", "tile_0000.png");
         my.sprite.player.setCollideWorldBounds(true);
 
         // Enable collision handling
         this.physics.add.collider(my.sprite.player, this.groundLayer);
-
+        
+        this.physics.add.overlap(my.sprite.player, this.winflagGroup, (obj1, obj2) => {
+this.win = true
+        })
          // Handle collision detection with coins
          this.physics.add.overlap(my.sprite.player, this.coinGroup, (obj1, obj2) => {
             //if a coin is collided with it will play the particle effect set
             my.vfx.collection = this.add.particles(obj2.x, obj2.y, "kenny-particles", {
                 frame: ['star_01.png', 'star_02.png'],
-                // TODO: Try: add random: true
                 scale: {start: 0.03, end: 0.1, random: true},
-                // TODO: Try: maxAliveParticles: 8,
+                
                 lifespan: 300,
                 maxAliveParticles: 8,
-                // TODO: Try: gravityY: -400,
+
+                
                 alpha: {start: 1, end: 0.1}, 
                 gravityY: -400,
+                
             });
            
-
             obj2.destroy(); // remove coin on overlap
             this.score += 1 // adds one point on collection of coins
-            this.text.destroy();//destroy text 
-        this.text = this.add.text(0, 0, 'Score:' + this.score, { fontFamily: 'Arial', fontSize: 18, color: '#ffffff' });//Updates text
+            
+           
+        this.text = this.add.text(my.sprite.player.x, my.sprite.player.y, this.score, { fontFamily: 'Arial', fontSize: 18, color: '#ffffff' });//Updates text
         //starts the visual effect and then stops it after .8  of a second
         my.vfx.collection.start();
 
         setTimeout(() => {
+            this.text.destroy();//destroy text 
             my.vfx.collection.stop(); 
             
-        }, 500);
+        }, 1000);
         
         
         });
@@ -214,16 +237,19 @@ class Platformer extends Phaser.Scene {
         }
 
         // player jump
-        // note that we need body.blocked rather than body.touching b/c the former applies to tilemap tiles and the latter to the "ground"
+        
         if(!my.sprite.player.body.blocked.down) {
             my.sprite.player.anims.play('jump');
         }
-        if(my.sprite.player.body.blocked.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
+        if(my.sprite.player.body.blocked.down && Phaser.Input.Keyboard.JustDown(cursors.space)) {
             my.sprite.player.body.setVelocityY(this.JUMP_VELOCITY);
         }
 
         if(Phaser.Input.Keyboard.JustDown(this.rKey)) {
             this.scene.restart();
+        }
+        if(this.win== true){
+            console.log('no')
         }
     }
 }
